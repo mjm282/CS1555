54a99dd96ba118ab8a2640b4c88b1d8f2c00aef5
Charles Smith <cas275@pitt.edu>
2016-11-14 21:21:59 -0500

    work

---
+++ adjustTicket.sql
@@ -0,0 +1,27 @@
+-- You should create a trigger, called adjustTicket, that adjusts the cost of a reservation when
+-- the price of one of its legs changes before the ticket is issued.
+
+CREATE OR REPLACE TRIGGER adjustTicket
+    AFTER UPDATE ON price
+    FOR EACH ROW
+    DECLARE 
+        hprice_change int;
+        lprice_change int;
+    BEGIN
+        -- finding the price differences 
+        SELECT :new.high_price - :old.high_price INTO hprice_change
+        FROM dual;
+
+        SELECT :new.low_price - :old.low_price INTO lprice_change
+        FROM dual;
+
+        -- I'm a bit iffy on when to use high vs low
+        UPDATE reservation 
+        SET cost = cost + hprice_change
+        WHERE start_city = :new.departure_city AND
+              end_city   = :new.arrival_city;
+
+    END;
+/
+
+    
