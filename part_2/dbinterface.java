0a4f304c5ed803fe3b2dc657d0e0a501a8288b12
Charles Smith <cas275@pitt.edu>
2016-11-28 11:57:36 -0500

    start here everyone\!

--- part_2/dbinterface.java
+++ part_2/dbinterface.java
@@ -15,6 +15,7 @@ public class dbinterface{
         passwrd = "pitt1787";
 
         try{
+            System.out.println("Connecting . . .");
             DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
             String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass";
             connection = DriverManager.getConnection(url, uname, passwrd);
@@ -25,7 +26,13 @@ public class dbinterface{
         }
 
         System.out.println("are you an admin? y/n");
-        char in = (char) System.in.read();
+        char in = '0';
+        try{
+            in = (char) System.in.read();
+        }
+        catch(Exception e2){
+            System.out.println("couldn't read that " + e2);
+        }
 
         if(in == 'y'){
             adminInterface();
@@ -38,10 +45,22 @@ public class dbinterface{
 
     public static void adminInterface(){
         System.out.println("~Admin menu~");
+        try{
+            connection.close();
+        }
+        catch(Exception e){
+            System.out.println("cannot close");
+        }
 
     }
     public static void userInterface(){
         System.out.println("User menu");
+        try{
+            connection.close();
+        }
+        catch(Exception e){
+            System.out.println("cannot close");
+        }
     }
-
 }
+
