4d817f4cd274853f47286e2b7eb547e35230130a
Sheppy <wgs11@pitt.edu>
2016-11-29 01:32:37 -0500

    partial 4 but too tired
    4 needs completing but brain is shot, pick it up tomorrow

--- part_2/dbinterface.java
+++ part_2/dbinterface.java
@@ -380,7 +380,28 @@ public class dbinterface{
                     
             }
             else if(in == '4'){
-
+                
+                try {
+                    //select * from flight f1 JOIN flight f2 on f1.arrival_city = f2.departure_city AND f1.airline_id = f2.airline_id;
+                    System.out.println("Find Routes");
+                    System.out.print("Please enter origin city: ");
+                    String origin = scan.next();
+                    System.out.print("Please enter destination city: ");
+                    String dest = scan.next();
+                    String directQuery = "SELECT flight_number, departure_city, arrival_city,departure_time,arrival_time FROM Flight WHERE departure_city = ? AND arrival_city = ?";
+                    PreparedStatement findDirect = connection.prepareStatement(directQuery);
+                    findDirect.setString(1,origin);
+                    findDirect.setString(2,dest);
+                    ResultSet rs = findDirect.executeQuery();
+                    while(rs.next()){
+                        System.out.println(rs);
+                    }
+                    String indirectQuery = "SELECT flight_number, departure_city, arrival_city,departure_time,arrival_time FROM Flight f1 JOIN Flight f2 on f1.arrival_city = f2.departure_city AND f1.airline_id = f2.airline_id WHERE departure_city = ? AND arrival_city = ?";
+                } catch (SQLException ex) {
+                    Logger.getLogger(dbinterface.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                    
+                
             }
             else if(in == '5'){
 
