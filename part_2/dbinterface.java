2adabddede379c96278a2bd436a79f449386848c
Charles Smith <cas275@pitt.edu>
2016-11-29 10:37:48 -0500

    finished U2

--- part_2/dbinterface.java
+++ part_2/dbinterface.java
@@ -409,7 +409,17 @@ public class dbinterface{
                     checkcust.setString(2, lname);
                     ResultSet rs = checkcust.executeQuery();
                     if (rs.next()){
-                        System.out.println("We should complete this function.");
+                        System.out.println(rs.getString(1) + " " 
+                                            + rs.getString(2) + " "
+                                            + rs.getString(3) + " "
+                                            + rs.getString(4) + " "
+                                            + rs.getString(5) + " "
+                                            + rs.getDate(6)   + " "
+                                            + rs.getString(7) + " "
+                                            + rs.getString(8) + " "
+                                            + rs.getString(9) + " "
+                                            + rs.getString(10)+ " "
+                                            + rs.getString(11));
                     }
                 } catch (SQLException ex) {
                     Logger.getLogger(dbinterface.class.getName()).log(Level.SEVERE, null, ex);
